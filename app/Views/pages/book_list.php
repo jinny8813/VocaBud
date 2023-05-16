5cea4be3f2d0c426d37909559e0352a6a7232ad5
chingyi lan <jinny8813@gmail.com>
2023-05-17 04:00:37 +0800

    book list card style and add class to a tags

--- app/Views/pages/book_list.php
+++ app/Views/pages/book_list.php
@@ -11,7 +11,7 @@
                     <div class="position-absolute top-100 start-50 translate-middle d-flex justify-content-between align-items-center w-100">
                         <div class="card w-75">
                             <div class="card-body">
-                                <div class="text-center fs-3">的個人主頁的個人主頁</div>
+                                <div class="text-center fs-3">書本列表</div>
                             </div>
                         </div>
                         <div>
@@ -26,15 +26,24 @@
         </div>
     </div>
     <div class="container py-5">
-        <div class="row justify-content-center m-3">
-            <div class="col-5 pt-5 row justify-content-end">
-                <div style="width:100px">
-                    <img class="img-fluid" src="<?= base_url('../../public/assets/images/icon.png') ?>" alt="">
+        <div class="row justify-content-center my-3">
+            <div class="col-md-10">
+
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-3 d-flex align-items-center justify-content-center bg_dark_blue rounded-start">
+                            <p class="text-center">000</p>
+                        </div>
+                        <div class="col-9 card-body">
+                            <div class="fs-5">
+                                <a href="" class="a_black stretched-link">的個人主頁的個人主頁</a>
+                            </div>
+                            <p class="ellipsis">的個人主頁的個人主頁的個人主頁的個人主頁的個人主頁的個人主頁</p>
+                            <div><i class="fa-fw fa-solid fa-swatchbook"></i>123</div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-7 pt-5">
-                <h2>LetsgoVoc</h2>
-                <p>Let's enjoy learning in an efficient way.</p>
+
             </div>
         </div>
     </div>
