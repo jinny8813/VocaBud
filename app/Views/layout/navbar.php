623c48a72f2e6c38cb9a32a326f01e67184fc460
chingyi lan <jinny8813@gmail.com>
2023-05-17 01:17:57 +0800

    update my banner title style in user home and add books link direction

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -13,7 +13,7 @@
     <div class="collapse navbar-collapse justify-content-end" id="navbar">
       <ul class="navbar-nav">
         <li class="nav-item ">
-          <a class="nav-link p-3" href="#">書本列表</a>
+          <a class="nav-link p-3" href="<?= base_url('/books') ?>">書本列表</a>
         </li>
         <li class="nav-item ">
           <a class="nav-link p-3" href="#">所有單字</a>
--- app/Views/pages/user_home.php
+++ app/Views/pages/user_home.php
@@ -4,12 +4,16 @@
     <div class="w-100">
         <img class="img-fluid" src="<?= base_url('../../public/assets/images/banner.jpg') ?>" alt="">
     </div>
-    <div class="container position-relative">
+    <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8 position-absolute top-100 start-50 translate-middle">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="text-center fs-3"><?= esc($nickname)?>的個人主頁</div>
+            <div class="col-md-8">
+                <div class="position-relative">
+                    <div class="position-absolute top-100 start-50 translate-middle d-flex justify-content-center align-items-center w-100">
+                        <div class="card w-100">
+                            <div class="card-body">
+                                <div class="text-center fs-3"><?= esc($nickname)?>的個人主頁</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -31,7 +35,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 col-6" style="background-color:#2c4c01">
-                <div class="text-center fs-5 py-5"><a href="#">書本列表</a></div>
+                <div class="text-center fs-5 py-5"><a href="<?= base_url('/books') ?>">書本列表</a></div>
             </div>
             <div class="col-md-3 col-6" style="background-color:#628100">
                 <div class="text-center fs-5 py-5"><a href="#">所有字卡</a></div>
