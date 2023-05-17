cadab4046b99f35b028b003ddb7a3beb81717707
chingyi lan <jinny8813@gmail.com>
2023-05-17 13:19:58 +0800

    per book page

---
+++ app/Models/CardModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class CardModel extends Model
+{
+    protected $table = 'cards';
+    protected $allowedFields = ['card_id','book_id','card_title','card_content','card_state','create_at',
+                'card_pronunciation','card_e_sentence','card_c_sentence','card_star','part_of_speech'];
+}
